87ef63ce4609fecaa54b5f8a7f9cfce702c5e1ec
Marcelo Silva <marcelorsdd@gmail.com>
2022-09-22 21:45:45 -0300

    Add - Function CloseModal

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let qtPizza = 1;
 const c = (c) => document.querySelector(c);
-const cs = (cs) => document.querySelector(cs);
+const cs = (cs) => document.querySelectorAll(cs);
 
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute("data-key", index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -11,11 +13,41 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.querySelector('a').addEventListener("click", (a) => {
         a.preventDefault();
+        qtPizza = 1;
+
+        let key = a.target.closest('.pizza-item').getAttribute("data-key");
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove("selected");
+        cs('.pizzaInfo--size').forEach(( size, indexSize) => {
+            if( indexSize == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[indexSize];
+        });
+
+        c('.pizzaInfo--qt').innerHTML = qtPizza;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = "flex";
-        c('.pizzaWindowArea').style.opacity = 1;
+        setTimeout( () => {
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
     })
 
     c('.pizza-area').append(pizzaItem);
-})
+});
+
+const closeModal = () => {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout( () => {
+        c('.pizzaWindowArea').style.display = "none";
+    }, 500);
+};
+
+cs('.pizzaInfo--cancelButton', '.pizzaInfo--cancelMobileButton').forEach( (item)=>{
+    item.addEventListener('click', closeModal);
+});
