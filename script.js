fdb239199b31bbc5a5210a5a337c6e36203802d6
Marcelo Silva <marcelorsdd@gmail.com>
2022-09-23 21:26:49 -0300

    Add - Price Modal

--- script.js
+++ script.js
@@ -98,11 +98,17 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 const updateCart = () => {
     if( cart.length > 0 ){
         c('aside').classList.add('show');
-        
         c('.cart').innerHTML = '';
+
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
 
         for(let i in cart){
             let pizzaItem = pizzaJson.find( (item)=> item.id == cart[i].id);
+
+            subTotal += pizzaItem.price * cart[i].qt;
+
             let cartItem = c('.cart--item').cloneNode(true);
 
             let pizzaSize;
@@ -125,10 +131,29 @@ const updateCart = () => {
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if( cart[i].qt > 1){
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
 
             c('.cart').append(cartItem);
         }
-        
+
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
     } else {
         c('aside').classList.remove('show');
     }
